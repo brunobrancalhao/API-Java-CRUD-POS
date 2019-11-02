b9b1637e31d5541b955d2df19072ab595a5b07d9
André Borges <hoilut@hotmail.com>
2019-11-02 18:01:21 -0300

    Adding Exceptions

--- src/main/java/br/com/informatica/Informatica/business/impl/CursoBusinessImpl.java
+++ src/main/java/br/com/informatica/Informatica/business/impl/CursoBusinessImpl.java
@@ -1,8 +1,11 @@
 package br.com.informatica.Informatica.business.impl;
 
 import br.com.informatica.Informatica.business.CursoBusiness;
+import br.com.informatica.Informatica.exception.InvalidParamsException;
 import br.com.informatica.Informatica.exception.NotFoundException;
 import br.com.informatica.Informatica.model.Curso;
+import br.com.informatica.Informatica.model.Materia;
+import br.com.informatica.Informatica.model.Turma;
 import br.com.informatica.Informatica.repository.CursoRepository;
 import br.com.informatica.Informatica.repository.MateriaRepository;
 import br.com.informatica.Informatica.repository.TurmaRepository;
@@ -40,6 +43,30 @@ public class CursoBusinessImpl implements CursoBusiness {
 
     @Override
     public Curso save(Curso curso) {
+        if (curso.getMaterias() == null) {
+            throw new InvalidParamsException("Erro: Materias são obrigatórias!");
+        }
+
+        if (curso.getTurmas() == null) {
+            throw new InvalidParamsException("Erro: Turmas são obrigatórias!");
+        }
+
+        for (Materia m : curso.getMaterias()) {
+            Optional<Curso> ot = cursoRepository.findById(m.getId());
+
+            if (ot.equals(Optional.empty())) {
+                throw new NotFoundException("Erro: Materia com ID:" + m.getId() + " não existe!");
+            }
+        }
+
+        for (Turma t : curso.getTurmas()) {
+            Optional<Turma> ot = turmaRepository.findById(t.getId());
+
+            if (ot.equals(Optional.empty())) {
+                throw new NotFoundException("Erro: Turma com ID:" + t.getId() + " não existe!");
+            }
+        }
+
         return cursoRepository.save(curso);
     }
 
@@ -51,11 +78,27 @@ public class CursoBusinessImpl implements CursoBusiness {
 
     @Override
     public Curso put(int id, Curso curso) {
+
+        if (curso.getMaterias() == null) {
+            throw new InvalidParamsException("Erro: Materias são obrigatórias!");
+        }
+
+        if (curso.getTurmas() == null) {
+            throw new InvalidParamsException("Erro: Turmas são obrigatórias!");
+        }
+
+        for (Materia m : curso.getMaterias()) {
+            Optional<Curso> ot = cursoRepository.findById(m.getId());
+
+            if (ot.equals(Optional.empty())) {
+                throw new NotFoundException("Erro: Materia com ID:" + m.getId() + " não existe!");
+            }
+        }
+
         try {
             Curso cur = cursoRepository.findById(id).get();
             curso.setId(id);
             return cursoRepository.save(curso);
-
         } catch (Exception ex) {
             return null;
         }
