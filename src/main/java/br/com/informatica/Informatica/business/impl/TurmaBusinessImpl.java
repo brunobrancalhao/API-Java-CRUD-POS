75a9dc736e93fc1f0bac87a0fc8ba94ab8d3f0c4
André Borges <hoilut@hotmail.com>
2019-11-04 01:15:50 -0300

    Fix

--- src/main/java/br/com/informatica/Informatica/business/impl/TurmaBusinessImpl.java
+++ src/main/java/br/com/informatica/Informatica/business/impl/TurmaBusinessImpl.java
@@ -1,8 +1,11 @@
 package br.com.informatica.Informatica.business.impl;
 
 import br.com.informatica.Informatica.business.TurmaBusiness;
+import br.com.informatica.Informatica.exception.InvalidParamsException;
 import br.com.informatica.Informatica.exception.NotFoundException;
+import br.com.informatica.Informatica.model.Curso;
 import br.com.informatica.Informatica.model.Turma;
+import br.com.informatica.Informatica.repository.CursoRepository;
 import br.com.informatica.Informatica.repository.TurmaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,14 +13,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TurmaBusinessImpl implements TurmaBusiness {
 
     private TurmaRepository turmaRepository;
+    private CursoRepository cursoRepository;
 
     @Autowired
-    public TurmaBusinessImpl(TurmaRepository turmaRepository) {
+    public TurmaBusinessImpl(TurmaRepository turmaRepository, CursoRepository cursoRepository) {
         this.turmaRepository = turmaRepository;
+        this.cursoRepository = cursoRepository;
     }
 
     @Override
@@ -32,6 +39,16 @@ public class TurmaBusinessImpl implements TurmaBusiness {
 
     @Override
     public Turma save(Turma turma) {
+        if(turma.getCurso() == null){
+            throw new InvalidParamsException("Erro: Curso é obrigatório!");
+        }
+
+        Optional<Curso> oc = cursoRepository.findById(turma.getCurso().getId());
+
+        if (oc.equals(Optional.empty())) {
+            throw new NotFoundException("Erro: Curso com ID: " + turma.getCurso().getId() + " não existe!");
+        }
+
         return turmaRepository.save(turma);
     }
 
@@ -43,6 +60,12 @@ public class TurmaBusinessImpl implements TurmaBusiness {
 
     @Override
     public Turma put(int id, Turma turma) {
+        Optional<Curso> oc = cursoRepository.findById(turma.getCurso().getId());
+
+        if (oc.equals(Optional.empty())) {
+            throw new NotFoundException("Erro: Curso com ID: " + turma.getCurso().getId() + " não existe!");
+        }
+
         try {
             Turma tur = turmaRepository.findById(id).get();
             turma.setId(id);
